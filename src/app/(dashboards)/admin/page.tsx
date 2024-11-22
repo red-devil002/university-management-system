bde07da545502f7e425a513629af72b16228b3b8
Swetang Pandit <swetangpandit@gmail.com>
2024-11-22 11:58:23 +0530

    Admin Dashboard UI and UX

--- src/app/(dashboards)/admin/page.tsx
+++ src/app/(dashboards)/admin/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import Announcements from "@/components/Announcements";
+import AttendanceChart from "@/components/AttendanceChart";
+import CountCharts from "@/components/CountCharts";
+import EventCalendar from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
 
-const Adminpage = () => {
+const AdminPage = () => {
   return (
-    <div className=''>
-      Admin Page
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountCharts />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Adminpage
+export default AdminPage;
--- src/app/(dashboards)/layout.tsx
+++ src/app/(dashboards)/layout.tsx
@@ -26,6 +26,7 @@ export default function DashboardLayout({
         {/* right */}
         <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
           <Navbar />
+          {children}
         </div>
       </div>
   );
