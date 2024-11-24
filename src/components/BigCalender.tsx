2695753d4a12bdd2b185e49799c8a9a87a0ae374
Swetang Pandit <swetangpandit@gmail.com>
2024-11-24 18:46:16 +0530

    Student Dashboard

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+import { calendarEvents } from "@/lib/data";
+import "moment/locale/hi";
+
+moment.locale('en-IN');
+const localizer = momentLocalizer(moment);
+
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  return (
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
+      style={{ height: "98%" }}
+      onView={handleOnChangeView}
+      min={new Date(2025, 1, 1, 8, 0, 0)}
+      max={new Date(2025, 1, 1, 17, 0, 0)}
+    />
+  );
+};
+
+export default BigCalendar;
